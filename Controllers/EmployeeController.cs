4b58111954c5b8e758f77bc7e28753afb295d0d7
unknown <Administrator@KANINI-LTP-479.gisbizdev.gisbiz.chennai.com>
2021-10-18 11:22:17 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAPI.EmployeeModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly dbContext db = new dbContext();
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee()
+        {
+            return Ok(await db.Employees.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee e)
+        {
+            db.Employees.Add(e);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(int id,Employee e)
+        {
+            db.Entry(e).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            db.Employees.Remove(db.Employees.Find(id));
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            Employee e=await db.Employees.FindAsync(id);
+            return Ok(e);
+        }
+    }
+}
---
+++ EmployeeModel/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FirstAPI.EmployeeModel
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
